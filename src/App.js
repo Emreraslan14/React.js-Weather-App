0ec0db9ad3ba7c423a1898d66682aaf398d8efda
Emre Eraslan <eraslan1494@gmail.com>
2024-05-19 15:09:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+console.log(process.env.REACT_APP_WEATHER_API);
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=${location}&days=4&aqi=yes&alerts=yes`
+        );
+
+        setWeather(response.data);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (location) {
+      fetchData();
+    }
+  }, [location]);
+
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="divBox">
+      <div className="app-container">
+        <h1 className="app-title">Weather App</h1>
+        <div className="inp-container">
+          <input
+            type="text"
+            className="inp-location"
+            placeholder="Please enter city name"
+            value={location}
+            onChange={handleLocationChange}
+          />
+        </div>
+      </div>
+
+      {weather && (
+        <div className="weather-container">
+          {weather.forecast.forecastday.map((day) => (
+            <div className="day-container" key={day.date}>
+              <h2 className="date">{day.date}</h2>
+              <img className="weather-icon" src={day.day.condition.icon} />
+              <p>{day.day.maxtemp_c}</p>
+              <p>{day.day.condition.text}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
